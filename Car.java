9509cc05b49aae303f0f1016eddd479a3fc4a8e5
fredrik1313 <fredrik.nyhlen@outlook.com>
2023-11-08 11:36:10 +0100

    Updated

--- Car.java
+++ Car.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Car {
+public class Car implements Movable {
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
@@ -8,11 +8,9 @@ public class Car {
     public String modelName; // The car model name
 
     public boolean turboOn;
-
     public void setTurboOn(){
         turboOn = true;
     }
-
     public void setTurboOff(){
         turboOn = false;
     }
@@ -20,28 +18,36 @@ public class Car {
     public int getNrDoors(){
         return nrDoors;
     }
-
     public double getEnginePower(){
         return enginePower;
     }
-
     public double getCurrentSpeed(){
         return currentSpeed;
     }
-
     public Color getColor(){
         return color;
     }
-
     public void setColor(Color clr){
         color = clr;
     }
-
     public void startEngine(){
         currentSpeed = 0.1;
     }
-
     public void stopEngine(){
         currentSpeed = 0;
+    }
+
+    public double direction = 0.0;
+    public double x = 0.0;
+    public double y = 0.0;
+
+    public void move(double duration){
+
+    }
+    public void turnLeft(double angle){
+        direction -= angle;
+    }
+    public void turnRight(double angle){
+        direction += angle;
     }
 }
---
+++ Movable.java
@@ -0,0 +1,5 @@
+public interface Movable {
+    public void move(double duration);
+    public void turnLeft(double angle);
+    public void turnRight(double angle);
+}
