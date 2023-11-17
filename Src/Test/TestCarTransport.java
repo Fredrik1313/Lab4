b1342bdc303302ceb87513b131bbfb08f40d6d96
fredrik1313 <fredrik.nyhlen@outlook.com>
2023-11-17 13:52:55 +0100

    Updated

---
+++ Src/Test/TestCarTransport.java
@@ -0,0 +1,55 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestCarTransport {
+    CarTransport Truck = new CarTransport();
+    Saab95 saab = new Saab95();
+    Volvo240 volvo = new Volvo240();
+
+    @Test
+    public void testGasWithOpenRamp(){
+        Truck.openRamp();
+        Truck.gas(0.5);
+        assertEquals(0, Truck.currentSpeed, 1e-6);
+    }
+
+    @Test
+    public void testOpenRampWhileMoving(){
+        Truck.gas(0.5);
+        Truck.openRamp();
+        assertFalse(Truck.rampOpen);
+    }
+
+    @Test
+    public void testLoadCar(){
+        Truck.openRamp();
+        Truck.loadCar(saab);
+        Truck.loadCar(volvo);
+        assertEquals(2, Truck.nCarsLoaded, 1e-6);
+    }
+
+    @Test
+    public void testLoadCarWithClosedRamp(){
+        Truck.closeRamp();
+        Truck.loadCar(volvo);
+        assertEquals(0, Truck.nCarsLoaded, 1e-6);
+    }
+
+    @Test
+    public void testUnloadCar(){
+        Truck.openRamp();
+        Truck.loadCar(saab);
+        Truck.unloadCar();
+        assertEquals(0, Truck.nCarsLoaded, 1e-6);
+    }
+
+    @Test
+    public void testLoadLimit(){
+        Truck.openRamp();
+        for (int i = 0; i < 20; i++){
+            Truck.loadCar(saab);
+        }
+        assertEquals(8, Truck.nCarsLoaded, 1e-6);
+    }
+}
